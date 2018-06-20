3b63d802ad2f6d8cee9d9078b74af7998afe1290
Jan Balangue <jantonio.balangue@gmail.com>
2018-06-20 15:30:35 -0700

    Add images. Modify App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,70 @@ import Button from "./components/Button";
 import './App.css';
 
 state = {
-  buttons =[
+  buttons = [
     {
-      imageUrl: 
+      imageSource: "../public/images/arya-stark.JPG",
+      isClicked: false
+    },
+    {
+      imageSource: "../public/images/brienne.JPG",
+      isClicked: false 
+    },
+    {
+      imageSource: "../public/images/cersei-lannister.JPG",
+      isClicked: false
+    },  
+    {
+      imageSource: "../public/images/daenerys.JPG",
+      isClicked: false
+    },
+    {
+      imageSource: "../public/images/gendry.JPG",
+      isClicked: false 
+    },
+    {
+      imageSource: "../public/images/hound.JPG",
+      isClicked: false
+    },
+    {
+      imageSource: "../public/images/jaime-lannister.JPG",
+      isClicked: false
+    },
+    {
+      imageSource: "../public/images/jon-snow.JPG",
+      isClicked: false 
+    },
+    {
+      imageSource: "../public/images/mountain.JPG",
+      isClicked: false
+    },  
+    {
+      imageSource: "../public/images/night-king.JPG",
+      isClicked: false
+    },
+    {
+      imageSource: "../public/images/tyrion-lannister.JPG",
+      isClicked: false 
+    },
+    {
+      imageSource: "../public/images/yara.JPG",
       isClicked: false
     }
   ],
 }
 
-renderButtons = (props) => {
+renderButtons = (props) =>
   props.buttons.map(button =>
-    <Button>
-      <Thumbnail />
+    <Button onClick={this.handleClick}>
+      <Thumbnail src={props.imageSource}/>
     </Button>
   )
+
+handleClick = event => {
+  event.preventDefault();
+  this.setState({
+    isClicked = true
+  });
 }
 
 class App extends Component {
